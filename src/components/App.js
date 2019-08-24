b1aa3b835da8f836fa3540c6f016287383f5482e
Anusha <anusha@Anushas-MacBook-Air.local>
2019-08-24 22:49:36 +0530

    add youtube client

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({
+      selectedVideo: video
+    });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="nine wide cloumn">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="seven wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
